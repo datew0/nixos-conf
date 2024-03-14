1f90f114fc8cbf9b7e3953d438d993c269656f2a
datew0 <datew0@untlab.ru>
2024-03-14 23:56:01 +0300

    Testing disko

--- disko-config.nix
+++ disko-config.nix
@@ -21,20 +21,22 @@
             size = "100G";
             content = {
               type = "btrfs";
+              mountpoint = "/";
+              mountOptions = [ "compress=zstd" "noatime" ];
               subvolumes = { 
-                  "@" = {
-                    mountpoint = "/";
-                    mountOptions = [ "compress=zstd" "noatime" ];
-                  };
-                  "@/home" = {
+                  # "@" = {
+                  #   mountpoint = "/";
+                  #   mountOptions = [ "compress=zstd" "noatime" ];
+                  # };
+                  "home" = {
                     mountpoint = "/home";
                     mountOptions = [ "compress=zstd" ];
                   };
-                  "@/nix" = {
+                  "nix" = {
                     mountpoint = "/nix";
                     mountOptions = [ "compress=zstd" "noatime" ];
                   };
-                  "@/log" = {
+                  "log" = {
                     mountpoint = "/var/log";
                     mountOptions = [ "compress=zstd" "noatime" ];
                   };
